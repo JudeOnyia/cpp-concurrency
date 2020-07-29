c1542b5b17394d01666d5a5f407176a68cfda637
root <judeonyia10@gmail.com>
2020-07-28 20:59:57 -0700

    1) Wrote helper function getZ to calculate z from the info given 2) Wrote helper function julia_ym to calculate the ym term given z 3) Wrote function to compute julia set 4) Wrote test for function and visualized result

--- app/test_julia_set.cpp
+++ app/test_julia_set.cpp
@@ -1,7 +1,36 @@
 #include "ra/julia_set.hpp"
-
+#include <complex>
+#include <boost/multi_array.hpp>
+#include <cstddef>
+#include <iostream>
+#include <fstream>
 
 int main(){
+	using Real = double;
+	using std::cout;
+	using std::endl;
+	std::complex<Real> bottom_left(-1.25,-1.25);
+	std::complex<Real> top_right(1.25,1.25);
+	std::complex<Real> c(0.37,-0.16);
+	int max_iters(255);
+	int num_threads(8);
+	int num_rows(512);
+	int num_cols(512);
+	boost::multi_array<int, 2> a(boost::extents[num_rows][num_cols]);
+	ra::fractal::compute_julia_set(bottom_left,top_right,c,max_iters,a,num_threads);
 
+	std::ofstream outFile("image.pnm");
+	outFile<<"P2 "<<num_cols<<" "<<num_rows<<" "<<max_iters<<"\n";
+	cout<<"Did signature"<<endl;
+	for(int row(num_rows-1); row>=0; --row){
+		cout<<"row: "<<row<<endl;
+		for(int col(0); col<num_cols; ++col){
+			if(col){ outFile<<" "; }
+			outFile<<(a[row][col]);
+		}
+		outFile<<"\n";
+	}
+	outFile.close();
+	
 
 }
